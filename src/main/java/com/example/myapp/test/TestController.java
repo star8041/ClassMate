5a249f5a4e0edebc98ff069405cca643ac013830
김민석 <mhn07001@naver.com>
2026-06-15 18:25:32 +0900

    header, sidebar 중간 저장

---
+++ src/main/java/com/example/myapp/test/TestController.java
@@ -0,0 +1,15 @@
+package com.example.myapp.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("userName", "홍길동");
+        return "test";
+    }
+}
