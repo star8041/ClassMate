2ae01763acd2090eac06cd7c20fc750d2a4f36c3
김민석 <mhn07001@naver.com>
2026-06-15 12:38:41 +0900

    init

---
+++ src/main/java/com/example/myapp/ClassmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmateApplication.class, args);
+	}
+
+}
